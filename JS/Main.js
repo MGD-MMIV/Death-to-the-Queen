26032125b1fa2e5466cf2495c0a01b146fe04c35
xXGothik-F8Xx <ajfolcarelli@email.neit.edu>
2024-11-13 12:57:29 -0500

    Update Main.js

--- JS/Main.js
+++ JS/Main.js
@@ -5,12 +5,23 @@ Game Setup
  3. frame rate
  4. animation timer runs main function 60 frames per second
 -------------------------------------------*/
-var c = document.querySelector(`MyCanvas`)
-var ctx = c.getContext(`2d`)
-var fps = 1000/60
-var timer = setInterval(main, fps)
+var c = document.querySelector(`myCanvas`);
+var ctx = c.getContext(`2d`);
+var fps = 1000/60;
+var timer = setInterval(main, fps);
+
+
+
 
 //Declaring character/enemy objects
+var c = document.getElementById("myCanvas");
+var img = new Image();
+img.src = "Image/Level.png";
+img.onload = function() 
+{
+    ctx.drawImage(img.src, 10, 10, 1300, 800);
+    
+};
 var avatar; 
 var bugs;
 var queen;
@@ -28,3 +39,5 @@ queen = new GameObject();
 avatarIdle1.onload = () =>{
     ctx.drawImage(avatarIdle1,this.x,this.y,100,100);
 }
+
+
